2799e8d2e1490260fc3bd6df06507bf952532e41
TomJimkesGit <tomjimkes@gmail.com>
2017-10-01 17:27:11 +0200

    data prprocessing begonnen en bijna af (moet er nu vandoor) ik ga hier morgen mee verder

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,15 +1,61 @@
 
-def processPassengers():
-    file = open("rawdata\\12a.csv")
-    grouped = []
-    header1 = ""
-    header2 = ""
-    for line in file:
-        sp = line.split(';')
-        if (sp[0] == ""): #first line
-            header1 = line
-        elif (sp[0] == "Trip"):
-            header2 = line
-        else:
-            time = sp[2]
+import math
 
+def processPassengers():
+    file = open("rawdata/12abuckets.csv")
+    grouped = []
+    header1 = []
+    header2 = []
+    dateBuckets = {}
+    for line in file:
+        sp = line.split(',')
+        if (sp[0] == ""): #first line
+            header1 = sp
+        elif (sp[0] == "Trip"):
+            header2 = sp
+        else:
+            date = sp[1].split('/')[0]
+            time = sp[-1].rstrip()
+            passengers = list(map(lambda x: int(x), sp[3:20]))
+            if(date not in dateBuckets):
+                dateBuckets[date] = {}
+            if(time not in dateBuckets[date]):
+                dateBuckets[date][time] = [0] * 18
+            for num in range(0,17):
+                dateBuckets[date][time][num] += passengers[num]
+
+    #now that passengers have been summed to segments for each date, we group by time
+    timeBuckets = {}
+
+    for key,value in dateBuckets.items():
+        for tkey,tvalue in value.items():
+            if(tkey in timeBuckets):
+                timeBuckets[tkey].append(tvalue)
+            else:
+                timeBuckets[tkey] = [tvalue]
+
+    #now that all times have been grouped together, we can average
+    averages = {}
+    for key,value in timeBuckets.items():
+        totalCount = len(value)
+        averages[key] = []
+        for num in range(0,17):
+            accum = 0
+            for li in value:
+                accum += li[num]
+            if accum > 0:
+                averages[key].append(int(math.ceil(accum / totalCount)))
+            else:
+                averages[key].append(0) #if there were no passengers in this segment any of the days
+
+    outputFile = open("processeddata/12aproc.csv", 'w')
+
+    #write headers
+    outputFile.write(','.join(header1[2:20]) + '\n')
+    outputFile.write(','.join(header2[2:20]) + '\n')
+    for key,value in averages.items():
+        passengers = ','.join(str(x) for x in value)
+        outputFile.write(key + "," + passengers + '\n')
+
+
+processPassengers()
