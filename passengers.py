dc54e7be7ecb0f0077b45f7b433c9055e40814c0
TomJimkesGit <tomjimkes@gmail.com>
2017-10-10 16:45:33 +0200

    passengers class af

--- passengers.py
+++ passengers.py
@@ -4,36 +4,42 @@ import numpy
 
 class passengerDist:
 
-    #This is set to true if testdata is used, in order to properly read the passenger count dictionaries
-    testData = False
+    #constructor
+    def __init__(self):
+        # This is set to true if testdata is used, in order to properly read the passenger count dictionaries
+        self.testData = False
 
-    #Direction PR -> CS
-    #PR has ID 0 and index 0 in the list of passenger counts
-    passengersA = {}
+        # Direction PR -> CS
+        # PR has ID 0 and index 0 in the list of passenger counts
+        self.passengersA = {}
 
-    #Direction CS -> PR
-    #same goes here for the indices of passenger counts
-    passengersB = {}
+        # Direction CS -> PR
+        # same goes here for the indices of passenger counts
+        self.passengersB = {}
 
-    #the references for ids
-    idRefs = {
-        "P+R Uithof": 0,
-        "WKZ": 1,
-        "UMC": 2,
-        "Heidelberglaan": 3,
-        "Padualaan": 4,
-        "Kromme Rijn": 5,
-        "Galgenwaard": 6,
-        "Vaartscherijn": 7,
-        "Centraal Station Centrumzijde": 8
-    }
+        self.disembarkingA = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
+        self.disembarkingB = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
 
-    lastArrivalsA = ["6:00"] * 9
+        # the references for ids
+        self.idRefs = {
+            "P+R Uithof": 0,
+            "WKZ": 1,
+            "UMC": 2,
+            "Heidelberglaan": 3,
+            "Padualaan": 4,
+            "Kromme Rijn": 5,
+            "Galgenwaard": 6,
+            "Vaartscherijn": 7,
+            "Centraal Station Centrumzijde": 8
+        }
 
-    lastArrivalsB = ["6:00"] * 9
+        self.lastArrivalsA = ["6:00"] * 9
 
-    def __init__(self): {}
+        self.lastArrivalsB = ["6:00"] * 9
 
+    #PUBLIC FUNCTIONS
+
+    #If a testfile is used, this function is called to instatiate the classvariables
     def createFromTestFile(self, csvFile):
         #Do use segments of 15 minutes to make programming easier
         self.testData = True
@@ -43,12 +49,23 @@ class passengerDist:
         self.passengersA = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
         self.passengersB = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
 
+        #these will be used to calculate the fractions of leaving passengers
+        disemA = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
+        disemB = {"6:00": [0] * 9, "7:00": [0] * 9, "9:00": [0] * 9, "16:00": [0] * 9, "18:00": [0] * 9}
+
+
         for line in file:
             sp = line.split(";")
             if sp[1] == "0":
                 self.passengersA[str(sp[2]) + ":00"][self.idRefs[sp[0]]] = int(float(sp[4]))
+                disemA[str(sp[2]) + ":00"][self.idRefs[sp[0]]] = int(float(sp[5]))
             elif sp[1] == "1":
-                self.passengersB[str(sp[2]) + ":00"][self.idRefs[sp[0]]] = (float(sp[4]))
+                self.passengersB[str(sp[2]) + ":00"][self.idRefs[sp[0]]] = int(float(sp[4]))
+                disemB[str(sp[2]) + ":00"][self.idRefs[sp[0]]] = int(float(sp[5]))
+
+        #instantiate the disembarking dictionaries
+        self.getDisembarkingRatios(disemA, 0)
+        self.getDisembarkingRatios(disemB, 1)
 
     #The empirical data instance requires two files
     def createFromEmpiricalData(self, fileA, fileB):
@@ -63,10 +80,23 @@ class passengerDist:
             sp = line.split(",")
             self.passengersB[sp[0]] = list(reversed(list(map(lambda x: int(x), sp[1:10])))) #the list has to be reversed due to the order of the stations
 
+        #Fill the disembarking dics for the empirical data
+        self.disembarkingA = {"6:00": [0, 0.143, 0.034, 0.00, 1.84, 0.157, 1.316, 2.661, 100],
+                           "7:00": [0, 0, 0.064, 0.005, 19.170, 0.154, 3.955, 8.454, 100],
+                           "9:00": [0, 0.143, 0.034, 0.00, 1.84, 0.157, 1.316, 2.661, 100],
+                           "16:00": [0, 0.305, 0.018, 0.00, 0.0682, 0.075, 0.902, 3, 100],
+                           "18:00": [0, 0.143, 0.034, 0.00, 1.84, 0.157, 1.316, 2.661, 100]}
+
+        self.disembarkingB = {"6:00": [100, 97.734, 79.790, 64.240, 51.796, 5.285, 6.256, 8.676, 0],
+                              "7:00": [100, 97.472, 80.021, 64.845, 53.836, 2.677, 3.472, 3.625, 0],
+                              "9:00": [100, 97.734, 79.790, 64.240, 51.796, 5.285, 6.256, 8.676, 0],
+                              "16:00": [100, 99.745, 69.401, 52.3, 43.090, 14.989, 10.572, 13.861,0],
+                              "18:00": [100, 97.734, 79.790, 64.240, 51.796, 5.285, 6.256, 8.676, 0]}
+
     #returns a quantity of passengers for a given stop at a given time
     #the last arrival is stored for each stop in each direction
     #direction == 0 --> A, direction == 1 --> B
-    def boarding(self, seconds, stationId, direction):
+    def embarkingPassengers(self, seconds, stationId, direction):
         hours = math.floor(seconds / 3600)
         minutes = math.floor((seconds - hours * 3600) / 60)
         hours += 6
@@ -109,6 +139,64 @@ class passengerDist:
 
         return boardingPassengers
 
+    #Returns a PERCENTAGE of passengers that will disembark
+    def disembarkingPassengers(self, hours, minutes, stationId, direction):
+        timeSegment = self.disembarkingTimeSegment(hours, minutes)
+        dicRef = self.disembarkingA if direction == 0 else self.disembarkingB
+        return dicRef[stationId][timeSegment]
+
+
+    #PRIVATE FUNCTIONS
+
+    #get the ratios for disembarking passengers
+    def getDisembarkingRatios(self, disem, direction):
+        if direction == 0:
+            embRef = self.passengersA
+            disembRef = self.disembarkingA
+        else:
+            embRef = self.passengersB
+            disembRef = self.disembarkingB
+
+        #in the case of direction B, we have to start summing at the end of the list
+        for key, value in disem.items():
+            for n in range(0, 9):
+                if(direction == 0):
+                    embarkingTotal = sum(embRef[key][0:(n+1)])
+                    disembarkingTotal = disem[key][0] if n == 0 else sum(disem[key][0:n])
+                else:
+                    embarkingTotal = sum(embRef[key][n + 1:9])
+                    disembarkingTotal = disem[key][0] if n == 0 else sum(disem[key][n + 1:9])
+
+                if embarkingTotal == 0 or disembarkingTotal == 0 or disem[key][n] == 0:
+                    disembRef[key][n] = 0
+                else:
+                    #The max is 100 per cent, but this can be exceded due to rounding errors
+                    disembRef[key][n] = min(100, disem[key][n] / (embarkingTotal - disembarkingTotal) * 100)
+
+    def spreadSegments(self, lastHours, lastMinutes, lastSegment, hours, minutes, segment, stationId, direction):
+        # set the correct dict to obtain passenger information
+        passengersRef = self.passengersB
+        if (direction == 0):
+            passengersRef = self.passengersA
+        startIndex = list(passengersRef).index(lastSegment)
+        endIndex = list(passengersRef).index(segment)
+
+        #first calc the remainders for the head and tail distributions
+        remainderLast = self.remainder(lastHours, lastMinutes, int(segment.split(":")[0]), int(segment.split(":")[1]))
+        remainderCur = self.remainder(int(segment.split(":")[0]), int(segment.split(":")[1]), hours, minutes)
+
+        # now add the passengers from both segments to get the total amount of passengers for the head and tail
+        boardingPassengers = self.getPassengers(remainderCur[0] * 60 + remainderCur[1], segment, stationId, direction)
+        boardingPassengers += self.getPassengers(remainderLast[0] * 60 + remainderLast[1], lastSegment, stationId,direction)
+
+        print(boardingPassengers)
+
+        #we need to draw from segments inbetween the indices and add these totals to the global total
+        for index in range(startIndex + 1, endIndex):
+            boardingPassengers += self.getPassengers(-1, list(passengersRef)[index], stationId, direction)
+
+        return boardingPassengers
+
     #returns the number of passengers
     #if samples is set to -1, we take samples for each discrete step in the timesegment
     def getPassengers(self, samples, timeSegment, stationId, direction):
@@ -147,59 +235,33 @@ class passengerDist:
     #returns the segment
     def toTimeSegment(self, hours, minutes):
         if self.testData:  #if the simulation is inst. with testdata, the segments differ
-            if hours < 7: return "6:00"
-            elif hours < 9: return "7:00"
-            elif hours < 16: return "9:00"
-            elif hours < 18: return "16:00"
-            else: return "18:00"
+            return self.disembarkingTimeSegment(hours, minutes)
         else:
             mins = math.floor(minutes / 15) * 15
             mins = str(mins) if (mins > 0) else "00"
             return ":".join([str(hours), mins])
 
-    #n.o. samples to be taken from a certain distribution
-    #hours2 and minutes2 are later
-    #returns a tuple
-    def remainder(self, hours, minutes, hours2, minutes2):
-        remHours = hours2 - hours
+    #Timesegments for disembarking passengers
+    #These are the same for testdata
+    def disembarkingTimeSegment(self, hours, minutes):
+        if hours < 7:
+            return "6:00"
+        elif hours < 9:
+            return "7:00"
+        elif hours < 16:
+            return "9:00"
+        elif hours < 18:
+            return "16:00"
+        else:
+            return "18:00"
 
-        if(hours == hours2):
-            remMinutes = minutes2 - minutes
-        else :
-            remMinutes = minutes2 + (60 - minutes)
-
-        return [remHours, remMinutes]
-
-    def spreadSegments(self, lastHours, lastMinutes, lastSegment, hours, minutes, segment, stationId, direction):
-        # set the correct dict to obtain passenger information
-        passengersRef = self.passengersB
-        if (direction == 0):
-            passengersRef = self.passengersA
-        startIndex = list(passengersRef).index(lastSegment)
-        endIndex = list(passengersRef).index(segment)
-
-        #first calc the remainders for the head and tail distributions
-        remainderLast = self.remainder(lastHours, lastMinutes, int(segment.split(":")[0]), int(segment.split(":")[1]))
-        remainderCur = self.remainder(int(segment.split(":")[0]), int(segment.split(":")[1]), hours, minutes)
-
-        # now add the passengers from both segments to get the total amount of passengers for the head and tail
-        boardingPassengers = self.getPassengers(remainderCur[0] * 60 + remainderCur[1], segment, stationId, direction)
-        boardingPassengers += self.getPassengers(remainderLast[0] * 60 + remainderLast[1], lastSegment, stationId,direction)
-
-        print(boardingPassengers)
-
-        #we need to draw from segments inbetween the indices and add these totals to the global total
-        for index in range(startIndex + 1, endIndex):
-            boardingPassengers += self.getPassengers(-1, list(passengersRef)[index], stationId, direction)
-
-        return boardingPassengers
 
 asd = passengerDist()
-#asd.createFromEmpiricalData("processeddata/12a.csv", "processeddata/12b.csv")
-asd.createFromTestFile("testdata/input-data-passengers-01.csv")
+asd.createFromEmpiricalData("processeddata/12a.csv", "processeddata/12b.csv")
+#asd.createFromTestFile("testdata/input-data-passengers-01.csv")
 
 
-passe = asd.boarding(3500, 3, 0)
+passe = asd.embarkingPassengers(55800, 3, 0)
 print(asd.lastArrivalsA, asd.lastArrivalsB)
 print(passe)
 
